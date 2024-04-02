186708d3c21d0d9904096453630024bc9c25996a
slim97397 <130625131+slim97397@users.noreply.github.com>
2024-04-01 23:04:12 -0500

    Add files via upload

---
+++ JS/JS.js
@@ -0,0 +1,190 @@
+function PanelL(){
+    let Panel = document.getElementById("Panel");
+    Panel.classList.toggle("activeL");
+}
+
+function Modal(){
+    let Modal = document.getElementById("modal-pelicula");
+    Modal.classList.toggle("activeL");
+}
+
+function Modal1(){
+    let Modal = document.getElementById("modal-pelicula1");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal2(){
+    let Modal = document.getElementById("modal-pelicula2");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal3(){
+    let Modal = document.getElementById("modal-pelicula3");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal4(){
+    let Modal = document.getElementById("modal-pelicula4");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal5(){
+    let Modal = document.getElementById("modal-pelicula5");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal6(){
+    let Modal = document.getElementById("modal-pelicula6");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal7(){
+    let Modal = document.getElementById("modal-pelicula7");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal8(){
+    let Modal = document.getElementById("modal-pelicula8");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal9(){
+    let Modal = document.getElementById("modal-pelicula9");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal10(){
+    let Modal = document.getElementById("modal-pelicula10");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal11(){
+    let Modal = document.getElementById("modal-pelicula11");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal12(){
+    let Modal = document.getElementById("modal-pelicula12");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal13(){
+    let Modal = document.getElementById("modal-pelicula13");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal14(){
+    let Modal = document.getElementById("modal-pelicula14");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal15(){
+    let Modal = document.getElementById("modal-pelicula15");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal16(){
+    let Modal = document.getElementById("modal-pelicula16");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal17(){
+    let Modal = document.getElementById("modal-pelicula17");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal18(){
+    let Modal = document.getElementById("modal-pelicula18");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal19(){
+    let Modal = document.getElementById("modal-pelicula19");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal20(){
+    let Modal = document.getElementById("modal-pelicula20");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal21(){
+    let Modal = document.getElementById("modal-pelicula21");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal22(){
+    let Modal = document.getElementById("modal-pelicula22");
+    Modal.classList.toggle("activeM");
+}
+
+function Modal23(){
+    let Modal = document.getElementById("modal-pelicula23");
+    Modal.classList.toggle("activeM");
+}
+
+
+
+
+
+
+function cont(){
+    let cont = document.getElementById("cont");
+    cont.classList.remove("cont1");
+    let cont1 = document.getElementById("cont");
+    cont1.classList.add("cont1-1");
+    let cont2 = document.getElementById("cont2");
+    cont2.classList.add("cont2");
+    let cont3 = document.getElementById("cont3");
+    cont3.classList.remove("cont3-3");
+    let cont33 = document.getElementById("cont3");
+    cont33.classList.add("cont3");
+    let act = document.getElementById("aventura");
+    act.classList.add("active");
+    let act1 = document.getElementById("animada");
+    act1.classList.remove("active");
+    let act2 = document.getElementById("drama");
+    act2.classList.remove("active");
+}
+
+function cont2(){
+    let cont = document.getElementById("cont");
+    cont.classList.remove("cont1-1");
+    let cont1 = document.getElementById("cont");
+    cont1.classList.add("cont1");
+    let cont2 = document.getElementById("cont2");
+    cont2.classList.remove("cont2");
+    let cont22 = document.getElementById("cont2");
+    cont22.classList.add("cont2-2");
+    let cont3 = document.getElementById("cont3");
+    cont3.classList.remove("cont3-3");
+    let cont33 = document.getElementById("cont3");
+    cont33.classList.add("cont3");
+    let act = document.getElementById("aventura");
+    act.classList.remove("active");
+    let act1 = document.getElementById("animada");
+    act1.classList.add("active");
+    let act2 = document.getElementById("drama");
+    act2.classList.remove("active");
+}
+
+function cont3(){
+    let cont = document.getElementById("cont");
+    cont.classList.remove("cont1-1");
+    let cont1 = document.getElementById("cont");
+    cont1.classList.add("cont1");
+    let cont2 = document.getElementById("cont2");
+    cont2.classList.remove("cont2-2");
+    let cont22 = document.getElementById("cont2");
+    cont22.classList.add("cont2");
+    let cont3 = document.getElementById("cont3");
+    cont3.classList.remove("cont3");
+    let cont33 = document.getElementById("cont3");
+    cont33.classList.add("cont3-3");
+    let act = document.getElementById("aventura");
+    act.classList.remove("active");
+    let act1 = document.getElementById("animada");
+    act1.classList.remove("active");
+    let act2 = document.getElementById("drama");
+    act2.classList.add("active");
+}
